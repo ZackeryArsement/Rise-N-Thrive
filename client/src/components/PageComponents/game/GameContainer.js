ed480ac84eb02aaa50d05a552e8db36c1848feeb
Zackery Arsement <zarsement@hotmail.com>
2022-04-26 13:56:17 -0500

    Updated css for mobile

--- client/src/components/PageComponents/game/GameContainer.js
+++ client/src/components/PageComponents/game/GameContainer.js
@@ -9,50 +9,49 @@ const Game = ({
     const [textColor, , theme] = useContext(ColorContext)
 
     return (
-        <div className={classes.gameTab}>
+        <div className={classes.gameTab} style={{ backgroundColor: theme }}>
             {games &&
                 games.map((game)=> (
-                    <div className={classes.container} style={theme === "#393939"
-                    ? { backgroundColor: 'rgb(41, 41, 41)', color: 'white' }
-                    : { backgroundColor:'rgb(41, 41, 41, 0.20)', color: 'black'}}
-                    key={game.title}>
-                        
-                        {/* Container Content */}
-                        {/* ----------------- */}
-
-                        {/* Game Title */}
-                        <h1 className={`${classes.title} ${classes.mb1}`} style={theme === "#393939" 
-                        ? { backgroundColor: `${textColor}52`, color: 'white' } 
-                        : { backgroundColor: `${textColor}99`, color: 'black' }}>
-
-                            {/* Title Text */}
-                            {game.title}
-                        </h1>
-                        
-                        {/* Game Image */}
-                        {game.href && game.href.includes('.html') ?
-                            <a href={game.href}>
-                                <img className={classes.img} src={game.img}/>
-                            </a>
+                        <div className={game.title === 'Lil\' Penguin' ? (`${classes.container} ${classes.penguin}`) : (classes.container) } style={theme === "#393939"
+                        ? { backgroundColor: 'rgb(41, 41, 41)', color: 'white' }
+                        : { backgroundColor:'rgb(41, 41, 41, 0.20)', color: 'black'}}
+                        key={game.title}>
                             
-                            : <NavLink
-                            to={game.href}> 
-                                <img className={classes.img} src={game.img}/>
-                            </NavLink>
-                        }
-
-
-                        {/* Game Stat */}
-                        <h1 className={`${classes.title} ${classes.mt1}`} style={theme === "#393939" 
-                        ? { backgroundColor: `${textColor}52`, color: 'white' } 
-                        : { backgroundColor: `${textColor}99`, color: 'black' }}>
-
-                            {/* Stat Text */}
-                            {game.unique} {game.stat}
-                        </h1>
-
-                    </div>
-
+                            {/* Container Content */}
+                            {/* ----------------- */}
+    
+                            {/* Game Title */}
+                            <h1 className={`${classes.title} ${classes.mb1}`} style={theme === "#393939" 
+                            ? { backgroundColor: `${textColor}52`, color: 'white' } 
+                            : { backgroundColor: `${textColor}99`, color: 'black' }}>
+    
+                                {/* Title Text */}
+                                {game.title}
+                            </h1>
+                            
+                            {/* Game Image */}
+                            {game.href && game.href.includes('.html') ?
+                                <a href={game.href}>
+                                    <img className={classes.img} src={game.img}/>
+                                </a>
+                                
+                                : <NavLink
+                                to={game.href}> 
+                                    <img className={classes.img} src={game.img}/>
+                                </NavLink>
+                            }
+    
+    
+                            {/* Game Stat */}
+                            <h1 className={`${classes.title} ${classes.mt1}`} style={theme === "#393939" 
+                            ? { backgroundColor: `${textColor}52`, color: 'white' } 
+                            : { backgroundColor: `${textColor}99`, color: 'black' }}>
+    
+                                {/* Stat Text */}
+                                {game.unique} {game.stat}
+                            </h1>
+    
+                        </div>
                 ))}
         </div>
     )
